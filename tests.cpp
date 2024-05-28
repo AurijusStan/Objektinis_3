2ef417036ded5ec60eff3614d74a68019dc076d3
AurijusStan <aurijus.stan@outlook.com>
2024-05-28 22:31:58 +0300

    dar testu

--- tests.cpp
+++ tests.cpp
@@ -71,7 +71,7 @@ TEST_CASE("Vector Resize")
         CHECK(v[3] == 5);
         CHECK(v[8] == 5);
         }
-TEST_CASE("Vector Reserve")
+TEST_CASE("Reserve/shrink_to_fit")
         {
         Vector<int> v;
         v.reserve(10);
@@ -79,6 +79,15 @@ TEST_CASE("Vector Reserve")
 
         v.reserve(5);
         CHECK(v.capacity() == 5);
+
+        for(int i=0; i<2; i++){
+                v.push_back(i);
+        }
+
+        v.shrink_to_fit();
+
+        CHECK(v.capacity() == 2);
+
         }
 TEST_CASE("Push_back")
         {
@@ -100,7 +109,6 @@ TEST_CASE("Pop_back")
         CHECK(v.getSize() == 5);
 
         v.pop_back();
-
         CHECK(v.getSize() == 4);
         }
 TEST_CASE("front/back/at")
@@ -113,17 +121,77 @@ TEST_CASE("front/back/at")
         CHECK(v[1] == 1);
         CHECK(v.front() == 0);
         CHECK(v.back() == 4);
-        CHECK(v.at(2) == 3);
+        CHECK(v.at(2) == 2);
         }
-TEST_CASE("front/back/at")
+TEST_CASE("iterators")
         {
-        Vector<int> v;
-        for(int i=0; i<5; i++){
-                v.push_back(i);
-        }
+        Vector<int> v{1, 2, 3};
+        CHECK(*v.begin() == 1);
+        CHECK(*(v.end() - 1) == 3);
 
-        CHECK(v[1] == 1);
-        CHECK(v.front() == 0);
-        CHECK(v.back() == 4);
-        CHECK(v.at(2) == 3);
+        CHECK(*v.rbegin() == 3);
+        CHECK(*(v.rend() - 1) == 1);
+        }
+TEST_CASE("insert")
+        {
+        Vector<int> v{1, 2, 3, 4, 5};
+
+        v.insert(v.begin()+2, 10);
+
+        CHECK(v.getSize() == 6);
+        CHECK(v[2] == 10);
+
+        Vector<int> b{9, 8, 7};
+
+        v.insert(v.begin()+2, b.begin(), b.end());
+
+        CHECK(v.getSize() == 9);
+        CHECK(v[2] == 9);
+        CHECK(v[5] == 10);
+
+        v.insert(v.end(), {1, 2});
+        CHECK(v.getSize() == 11);
+        CHECK(v[9] == 1);
+        CHECK(v[10] == 2);
+        }
+TEST_CASE("erase")
+        {
+        Vector<int> v{1, 2, 3};
+
+        v.erase(v.begin()+1);
+
+        CHECK(v.getSize() == 2);
+        CHECK(v[1] == 3);
+
+        v.erase(v.begin(), v.end());
+
+        CHECK(v.getSize() == 0);
+        }
+TEST_CASE("swap")
+        {
+        Vector<int> v{1, 2, 3};
+        Vector<int> b{9, 8};
+
+        v.swap(b);
+
+        CHECK(v.getSize() == 2);
+        CHECK(v[0] == 9);
+
+        CHECK(b.getSize() == 3);
+        CHECK(b[2] == 3);
+        }
+TEST_CASE("clear")
+        {
+        Vector<int> v{1, 2, 3};
+        v.clear();
+        CHECK(v.getSize() == 0);
+        CHECK(v.empty() == true);
+        }
+TEST_CASE("emplace")
+        {
+        Vector<int> v{1, 2, 3};
+
+        v.emplace_back(6);
+        CHECK(v.getSize() == 4);
+        CHECK(v.back() == 6);
         }
